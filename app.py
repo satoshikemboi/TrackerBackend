32c53373c294ca658bc120def5038177f7cb2ef0
satoshikemboi <satoshikemboi@gmail.com>
2025-10-31 06:32:09 +0300

    trying to fix CORS

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ jwt = JWTManager()
 
 def create_app():
     app = Flask(__name__)
-    CORS(app, origins=["https://carbon-tracker-sand.vercel.app/"])
+    CORS(app, origins=["https://carbon-tracker-sand.vercel.app"])
 
     # Config
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tracker.db'
